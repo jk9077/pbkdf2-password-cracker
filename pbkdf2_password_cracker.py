7089ce7f995e91cd6866434a249e47484749df9d
jk9077 <betterman00@naver.com>
2026-04-03 22:21:44 +0900

    add speacial chars and username in table

--- pbkdf2_password_cracker.py
+++ pbkdf2_password_cracker.py
@@ -11,21 +11,41 @@ with open("hashedPasswords.txt", "r") as file:
             "hashed_password": hashed_password
         })
 
-words = ["password", "qwer", "hello", "apple", "ajou", "user", "abcd", "abc"]
+words = ["password", "qwer", "qwert", "hello", "apple", "ajou", "user", "abcd", "abc", "asd", 
+         "asdf", "asdfg", "my", "pass", "admin", "qwerty", "zxcv"]
 
-nums = ["1234", "0000", "1111", "2026", "2222"]
+nums = ["1234", "0000", "1111", "2026", "2222", "12", "123", "12345", "123456"]
+for i in range(10):
+    nums.append(str(i))
+
+special_chars = ["!", "@", "#", "!!", "@@", "##", "*", "**"]
+
 
 possible_passwords = []
+
+
 
 salt = b"ajou"
 
 for word in words:
     for num in nums:
         possible_passwords.append(word+num)
+        possible_passwords.append(word.capitalize()+num)
+        for special in special_chars:
+            possible_passwords.append(word+num+special)
+            possible_passwords.append(word.capitalize()+num+special)
 
-for password in possible_passwords:
-    hashed = hashlib.pbkdf2_hmac("sha1", password.encode(), salt, 500, 16).hex()
+for user in users:
+    for num in nums:
+        possible_passwords.append(user["username"]+num)
+        possible_passwords.append(user["username"].capitalize()+num)
+        for special in special_chars:
+            possible_passwords.append(user["username"]+num+special)
+            possible_passwords.append(user["username"].capitalize()+num+special)
+
+with open("Passwords.txt", "w") as result:
     for user in users:
-        if hashed == user["hashed_password"]:
-            with open("Passwords.txt", "w") as result:
-                result.write(f"{user["number"]}:{user["username"]}:{hashed}")
+        for password in possible_passwords:
+            hashed = hashlib.pbkdf2_hmac("sha1", password.encode(), salt, 500, 16).hex()
+            if hashed == user["hashed_password"]:
+                result.write(f"{user["number"]}:{user["username"]}:{password}\n")

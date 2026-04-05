f62ff65bb35c82e2b230f2eddc360705da7d544b
jk9077 <betterman00@naver.com>
2026-04-05 21:14:56 +0900

    add more password tables

--- pbkdf2_password_cracker.py
+++ pbkdf2_password_cracker.py
@@ -1,15 +1,17 @@
 import hashlib
 
-def try_password(candidate):
-    hashed = hashlib.pbkdf2_hmac("sha1", candidate.encode(), salt, 500, 16).hex()
-
-    if hashed in target_hashes:
-        for number, username in hash_to_users[hashed]:
-            found_passwords.add((number, username, candidate))
-
 users = []
 target_hashes = set()
 hash_to_users = {}
+salt = b"ajou"
+found_passwords = set()
+
+def try_password(password):
+    hashed = hashlib.pbkdf2_hmac("sha1", password.encode(), salt, 500, 16).hex()
+
+    if hashed in target_hashes:
+        for number, username in hash_to_users[hashed]:
+            found_passwords.add((number, username, password))
 
 with open("hashedPasswords.txt", "r") as file:
     for line in file:
@@ -36,17 +38,17 @@ for user in users:
 words = [
     "password", "qwer", "qwert", "hello", "apple", "ajou", "user",
     "abcd", "abc", "asd", "asdf", "asdfg", "my", "pass",
-    "admin", "qwerty", "zxcv"
-]
+    "admin", "qwerty", "zxcv", "apple", "banana", "dragon", "aaaa", "bbbb", "cccc", "dddd",
+    "test", "login", "love", "chicken", "maple", "sun", "star", "jk", "good", "bad", "star",
+    "guest", "root", "asdfgh", "dog", "cag"]
 
-nums = ["1234", "0000", "1111", "2026", "2222", "12", "123", "12345", "123456"]
+nums = ["1234", "0000", "1111", "2026", "2222", "12", "123", "12345", "123456", 
+        "3333", "4444", "5555", "00", "2000", "2025", "2024", "2023", "2020", "2021", "2022",
+        "11", "22", "33", "44", "55"]
 for i in range(10):
     nums.append(str(i))
 
 special_chars = ["!", "@", "#", "!!", "@@", "##", "*", "**"]
-
-salt = b"ajou"
-found_passwords = set()
 
 
 for word in words:
@@ -56,7 +58,7 @@ for word in words:
 
         for special in special_chars:
             try_password(word + num + special)
-            try_password(word.capitalize() + num + special)
+            try_password(word.capitalize() + num + special) 
 
 for user in users:
     username = user["username"]
@@ -70,5 +72,5 @@ for user in users:
             try_password(username.capitalize() + num + special)
 
 with open("Passwords.txt", "w") as result:
-    for number, username, password in found_passwords:
+    for number, username, password in sorted(found_passwords, key=lambda x: int(x[0])):
         result.write(f"{number}:{username}:{password}\n")
